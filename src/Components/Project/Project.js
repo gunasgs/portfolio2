2f99079f040bad3d9df231d9a3b2e8c9542216df
GUNA SGS <gunasgs357@gmail.com>
2022-08-04 17:09:51 +0530

    add

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -5,12 +5,17 @@ function Project() {
     {
       title: "Hotel Management",
       poster: "https://i.im.ge/2022/08/02/FA3QN9.hotel-booking.jpg",
-      link: "https://project-hotel-management.netlify.app",
+
+      frontend: "https://github.com/gunasgs/Project-Hotel-Management-Frontend",
+      backend: "https://github.com/gunasgs/Project-Hotel-Management-Backend",
+      live: "https://project-hotel-management.netlify.app",
     },
     {
       title: "Netflix-clone",
       poster: "https://i.im.ge/2022/08/02/FA52Qx.netflix.jpg",
-      link: "https://stirring-pastelito-40ba45.netlify.app",
+      live: "https://stirring-pastelito-40ba45.netlify.app",
+      frontend: "https://github.com/gunasgs/netflix-clone",
+      backend: "https://github.com/gunasgs/Netflix-Backend",
     },
   ];
   return (
@@ -30,14 +35,37 @@ function Project() {
                   data-aos-delay="100"
                 >
                   <div class="card icon-box" style={{ width: "18rem" }}>
-                    <a href={e.link} target="_blank">
+                    <a href={e.link} target="_blank" rel="noreferrer">
                       <img class="card-img-top" src={e.poster} alt="hotel" />{" "}
                     </a>
 
                     <h5 class="card-title text-center mt-3">{e.title}</h5>
-                  </div>
-                  <div className="project-desc">
-                    <h5>Live</h5>
+                    <div className="btn-group  ">
+                      <a
+                        href={e.frontend}
+                        rel="noreferrer"
+                        target="_blank"
+                        class="btn btn-outline-light btn-sm text-dark "
+                      >
+                        <i class="bi bi-github"></i> Front-End
+                      </a>
+                      <a
+                        href={e.backend}
+                        rel="noreferrer"
+                        target="_blank"
+                        class="btn btn-outline-light btn-sm text-dark"
+                      >
+                        <i class="bi bi-github"></i> Back-End
+                      </a>
+                      <a
+                        href={e.live}
+                        target="_blank"
+                        rel="noreferrer"
+                        class="btn btn-outline-light btn-sm text-dark"
+                      >
+                        <i class="bi bi-link-45deg me-2"></i>Live
+                      </a>
+                    </div>
                   </div>
                 </div>
               );
