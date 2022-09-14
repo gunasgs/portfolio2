5570bbe8274468253c59db8aeb3a36aef7693940
GUNA SGS <gunasgs357@gmail.com>
2022-09-14 14:15:39 +0530

    addd

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -23,75 +23,86 @@ function Project() {
 
       frontend: "https://github.com/gunasgs/netflix-clone",
       backend: "https://github.com/gunasgs/Netflix-Backend",
-      des: "Developed MERN Stack Application For  Movie Details With CRUD Function ,Where User Can Register And Login .Which Include Play Option In Netlify Movies",
+      des: "Developed MERN Stack Application For  Movie Details With CRUD Function Where User Can Register And Login Which Include Play Option In Netlify Movies",
     },
   ];
   return (
     <>
       <section id="project" class="skills">
         <div class="container" data-aos="fade-up">
-          <div class="section-title  pt-4 mb-5  ">
+          <div class="section-title   mb-5  ">
             <h2 className="mt-5">Project</h2>
           </div>
 
           <div class="row ">
             {project.map((e) => {
               return (
-                <div class="col-lg-6   col-md-12 col-sm-12 mx-auto project card">
-                  <div className="project-img imgBox">
-                    <img className="project-img" src={e.poster} alt="hotel" />
-                    <img className="project-img " src={e.poster2} alt="hotel" />
-                  </div>
-                  <div className="project-desc details">
-                    <div className="content">
-                      <h4 class="card-title text-center  mt-3">{e.title}</h4>
-                      <p class="des">{e.des}</p>
-                      <div class="project-icon">
-                        <div>
-                          <i class="devicon-react-original-wordmark colored iconn"></i>
+                <div class=" col-lg-6   col-md-12 col-sm-12 card-container">
+                  <div class="card-1">
+                    <div className="project-img imgBox">
+                      <img
+                        className="project-img "
+                        src={e.poster}
+                        alt="hotel"
+                      />
+                      <img
+                        className="project-img  "
+                        src={e.poster2}
+                        alt="hotel"
+                      />
+                    </div>
+
+                    <div className="project-desc details">
+                      <div className="content">
+                        <h4 class="card-title text-center  mt-3">{e.title}</h4>
+                        <p class="des">{e.des}</p>
+                        <div class="project-icon">
+                          <div>
+                            <i class="devicon-react-original-wordmark colored iconn"></i>
+                          </div>
+                          <div>
+                            <i class={` ${e.icon} colored iconn`}></i>
+                          </div>
+                          <div>
+                            <i class="devicon-mongodb-plain-wordmark colored iconn"></i>
+                          </div>
+                          <div>
+                            <img
+                              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
+                              alt="node"
+                              class="img-icon colored iconn"
+                            />
+                          </div>
+                          <div>
+                            <i class="devicon-express-original-wordmark colored iconn"></i>
+                          </div>
                         </div>
-                        <div>
-                          <i class={` ${e.icon} colored iconn`}></i>
+                        <div className="btn-group  ">
+                          <a
+                            href={e.frontend}
+                            rel="noreferrer"
+                            target="_blank"
+                            class="btn btn-sm"
+                          >
+                            <i class="bi bi-github"></i> Front-End
+                          </a>
+                          <a
+                            href={e.backend}
+                            rel="noreferrer"
+                            target="_blank"
+                            class="btn btn-sm"
+                          >
+                            <i class="bi bi-github"></i> Back-End
+                          </a>
+                          <a
+                            href={e.live}
+                            target="_blank"
+                            rel="noreferrer"
+                            class="btn btn-sm "
+                          >
+                            <i class="bi bi-link-45deg me-2"></i>Live
+                          </a>
                         </div>
-                        <div>
-                          <i class="devicon-mongodb-plain-wordmark colored iconn"></i>
-                        </div>
-                        <div>
-                          <img
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
-                            alt="node"
-                            class="img-icon colored iconn"
-                          />
-                        </div>
-                        <div>
-                          <i class="devicon-express-original-wordmark colored iconn"></i>
-                        </div>
-                      </div>
-                      <div className="btn-group  ">
-                        <a
-                          href={e.frontend}
-                          rel="noreferrer"
-                          target="_blank"
-                          class="btn btn-sm"
-                        >
-                          <i class="bi bi-github"></i> Front-End
-                        </a>
-                        <a
-                          href={e.backend}
-                          rel="noreferrer"
-                          target="_blank"
-                          class="btn btn-sm"
-                        >
-                          <i class="bi bi-github"></i> Back-End
-                        </a>
-                        <a
-                          href={e.live}
-                          target="_blank"
-                          rel="noreferrer"
-                          class="btn btn-sm "
-                        >
-                          <i class="bi bi-link-45deg me-2"></i>Live
-                        </a>
                       </div>
                     </div>
                   </div>
